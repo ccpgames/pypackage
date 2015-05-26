d086a2895addef50162fd3dcb2748adda4ac7509
a-tal <github@talsma.ca>
2015-05-26 11:54:46 -0700

    0.0.9 - automatic conversion from string -> list for single strings - extras_require can now be supplied with feature_requires, individual features will override their definitions in extras_require if they exist in both places - be less strict about the regex pattern's whitespace for finding author, version, etc

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def long_description():
 
 setup(
     name="pypackage",
-    version="0.0.8",
+    version="0.0.9",
     author="Adam Talsma",
     author_email="se-adam.talsma@ccpgames.com",
     url="http://ccpgames.github.io/pypackage",
