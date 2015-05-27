c8e67442a984b7c87d7eb5a38013226b11014a58
a-tal <github@talsma.ca>
2015-05-27 10:24:25 -0700

    0.1.1 - adds unicode support to guessed/found in files attributes like author, email, etc - can now cleanly exit the interactive build session with ^C or ^D

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def long_description():
 
 setup(
     name="pypackage",
-    version="0.1.0",
+    version="0.1.1",
     author="Adam Talsma",
     author_email="se-adam.talsma@ccpgames.com",
     url="http://ccpgames.github.io/pypackage",
