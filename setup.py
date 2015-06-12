38c12aec2e5f19882e4108608097b4107b30273e
a-tal <github@talsma.ca>
2015-06-12 08:37:30 -0700

    0.1.5 - typeerror in data_files enforcement

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def long_description():
 
 setup(
     name="pypackage",
-    version="0.1.4",
+    version="0.1.5",
     author="Adam Talsma",
     author_email="se-adam.talsma@ccpgames.com",
     url="http://ccpgames.github.io/pypackage",
