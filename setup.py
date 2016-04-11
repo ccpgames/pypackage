6e008152bb9c8fb13eebef8065a6340b0d14c6bf
a-tal <github@talsma.ca>
2016-04-11 10:04:46 -0700

    0.2.0 - pypackage will fatally exit on reading an invalid meta file

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def long_description():
 
 setup(
     name="pypackage",
-    version="0.1.9",
+    version="0.2.0",
     author="Adam Talsma",
     author_email="se-adam.talsma@ccpgames.com",
     url="http://ccpgames.github.io/pypackage",
